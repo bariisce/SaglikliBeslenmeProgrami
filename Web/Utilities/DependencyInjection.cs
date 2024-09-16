7c412108250a608938f172e97be4eae66beb04cb
barisscl <barisceylan907@gmail.com>
2024-09-16 12:18:12 +0300

    dosya dizinleri oluşturuldu

---
+++ Web/Utilities/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Data.Postgres;
+
+namespace Web.Utilities
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddProjectServices(this IServiceCollection services)
+        {
+            // Burada DI olarak eklenecek servisleri kaydediyoruz. 
+            // Örneğin, IRepository ve IUnitOfWork gibi servisleri ekleyebilirsiniz. 
+
+            return services;
+        }
+    }
+}
